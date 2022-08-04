a1407a98afacaaa4348896ad1c59f2273790ff34
Murilo Alves Melo <83835393+murilo-alvesmelo@users.noreply.github.com>
2022-08-04 15:42:58 -0300

    Update database.module.ts

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -8,10 +8,10 @@ import { User } from 'src/users/entity/users.entity';
         ConfigModule.forRoot({ isGlobal: true }),
         TypeOrmModule.forRoot({
             type: 'postgres',
-            host: 'localhost',
+            host: 'ec2-3-213-228-206.compute-1.amazonaws.com',
             port: 5432,
-            username: 'postgres',
-            password: '1234',
+            username: 'yrgamatheyqqvq',
+            password: '3c8ddc7051ef5164a73acc2967c8dbc3867395a41fa533fd944c6a7a20f1c646',
             autoLoadEntities: true,
             synchronize: true,
         }),
